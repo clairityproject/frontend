5d6a9218c58d42eecacaf42f8103a36e44df7556
Catherine Cheng <chengcatherine91@gmail.com>
2014-04-13 19:03:44 -0400

    about page changes
    about page changes

--- js/about.js
+++ js/about.js
@@ -1,1 +1,25 @@
-$('.carousel').carousel()
+$(function() {
+
+	$('body').scrollspy({ target: '.collapse.navbar-collapse' });
+
+	var handlerIn = function(evt) {
+		var featuresTile = $(evt.currentTarget).closest('.features-tile');
+		var leftPixels = featuresTile.attr('data-left');
+		featuresTile.animate({
+			width: '323px',
+			height: '322px',
+			left: leftPixels,
+			top: '-40px'
+		});
+		featuresTile.css('z-index', 1500)
+	};
+	var handlerOut = function(evt) {
+		var featuresTile = $(evt.currentTarget).closest('.features-tile');
+		featuresTile.animate({
+		}).removeAttr('style');
+	};
+	
+	$('.features-tile-cap').hover(handlerIn, handlerOut);
+
+
+});
