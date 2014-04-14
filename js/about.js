a595d9ec2b68abd493099667f792a7a471c0a67f
Catherine Cheng <chengcatherine91@gmail.com>
2014-04-13 21:28:52 -0400

    understanding network section
    understanding network section

--- js/about.js
+++ js/about.js
@@ -2,24 +2,25 @@ $(function() {
 
 	$('body').scrollspy({ target: '.collapse.navbar-collapse' });
 
-	var handlerIn = function(evt) {
-		var featuresTile = $(evt.currentTarget).closest('.features-tile');
-		var leftPixels = featuresTile.attr('data-left');
-		featuresTile.animate({
-			width: '323px',
-			height: '322px',
-			left: leftPixels,
-			top: '-40px'
-		});
-		featuresTile.css('z-index', 1500)
-	};
-	var handlerOut = function(evt) {
-		var featuresTile = $(evt.currentTarget).closest('.features-tile');
-		featuresTile.animate({
-		}).removeAttr('style');
-	};
+
+	// var handlerIn = function(evt) {
+	// 	var featuresTile = $(evt.currentTarget).closest('.features-tile');
+	// 	var leftPixels = featuresTile.attr('data-left');
+	// 	featuresTile.animate({
+	// 		width: '323px',
+	// 		height: '322px',
+	// 		left: leftPixels,
+	// 		top: '-40px'
+	// 	});
+	// 	featuresTile.css('z-index', 1500)
+	// };
+	// var handlerOut = function(evt) {
+	// 	var featuresTile = $(evt.currentTarget).closest('.features-tile');
+	// 	featuresTile.animate({
+	// 	}).removeAttr('style');
+	// };
 	
-	$('.features-tile-cap').hover(handlerIn, handlerOut);
+	// $('.features-tile-cap').hover(handlerIn, handlerOut);
 
 
 });
