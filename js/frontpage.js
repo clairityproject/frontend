4cfce1fc9268da2e198276c6b5678ae73621ab52
Sharone Small <sharonemichal@gmail.com>
2014-04-29 20:07:40 -0400

    Trying to reduce lag time
    Errors

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,31 +1,36 @@
 // In Front End
 
-//TO DO:
-//Fix map bounds - when zooms, doesn't center
+//My TO DO by Thursday:
+//Fix PM once Derek determines what to do
+//fix time delay of nodes
 //Last updated
-//Only get latest datapoint
+//don't show malfunctioning nodes
+//Initial explanation
+//Fix map bounds - when zooms, doesn't center
+//concentration units - once David is done callibrating
+//fix location margin
+
+//Others
+//color legend
+
+//At some later date
+//site label on graphs?????
 //map: Buildings from whereis.mit.edu ?
 
-//color legend
-//concentration units
-//site label on graphs?????
 
-//GRAPHS
-
-var serverNodesURL = "http://clairity.mit.edu/api/v1/node/?limit=0";
-var serverDataURL = "http://clairity.mit.edu/latest/all/";
+var serverURL = "http://clairity.mit.edu/latest/all/";
 
 var sensors = [];
 var new_sensor;
 var nodesDrawn = false;
-var update_int = 15000; //milliseconds
+var update_int = 15000; //milliseconds, 15 seconds
 
 var mapBig = true;
 
-var alpha1_thresholds = [100, 500, 900, 1300, 1500];
-var alpha2_thresholds = [100, 500, 900, 1300, 1500];
-var alpha3_thresholds = [100, 500, 900, 1300, 1500];
-var alpha4_thresholds = [100, 500, 900, 1300, 1500];
+var alpha1_thresholds = [100, 500, 900, 1300, 1500]; //CO
+var alpha2_thresholds = [100, 500, 900, 1300, 1500]; //NO
+var alpha3_thresholds = [100, 500, 900, 1300, 1500]; //NO2
+var alpha4_thresholds = [100, 500, 900, 1300, 1500]; //O3
 var pm25_thresholds = [100, 500, 900, 1300, 1500];
 var pm10_thresholds = [100, 500, 900, 1300, 1500];
 var alpha_thresholds = [alpha1_thresholds, alpha2_thresholds, alpha3_thresholds, alpha4_thresholds, pm25_thresholds, pm10_thresholds];
@@ -43,25 +48,21 @@ function sensor(lat,lon,location,id,in_out) {
 	this.color = 0; // 0 = green, 1 = yellow, 2 = orange, 3 = red
 	this.pm25 = null;
 	this.pm10 = null;
-	
 }
 
 function RequestNodes() {
-	$.getJSON(serverNodesURL, function (data) {
-		for(i=0; i<data["objects"].length; i++){
-				new_sensor = new sensor(data["objects"][i]["latitude"],data["objects"][i]["longitude"],data["objects"][i]["name"],data["objects"][i]["node_id"],data["objects"][i]["indoor"]);
-    			sensors.push(new_sensor);
-		}
-		nodesDrawn = true;
-	});
-}
-
-var date_separators = [" ","-",":"];
-
-function RequestDatapoints() {
-	if(nodesDrawn){
-		$.getJSON(serverDataURL, function (data) {
+	$.getJSON(serverURL, function (data) {
+		if(!nodesDrawn){
 			for(i=0; i<data.length; i++){
+				new_sensor = new sensor(data[i]["latitude"],data[i]["longitude"],data[i]["name"],data[i]["node_id"],data[i]["indoor"]);
+	    		sensors.push(new_sensor);
+			}
+			drawNodes();
+			//console.log(sensors);
+			nodesDrawn=true;
+		}
+		if(nodesDrawn){
+			for(i=0; i<sensors.length; i++){
 				sensors[i].color = 0;
 				for(j=1; j<5; j++){
 					addAlphasenseData(i,j,data);
@@ -71,9 +72,9 @@ function RequestDatapoints() {
 				var tempDate = data[i]["last_modified"].split(/[\s*\-\s*,":"]/,5);
 				sensors[i].lastUpdated = new Date(tempDate[0],tempDate[1],tempDate[2],tempDate[3],tempDate[4]);
 			}
-			setColor();
-		});
-	}
+		}
+	});
+	setColor();
 }
 
 function addAlphasenseData(i,j,data){
@@ -137,41 +138,17 @@ function displaySidebar(i){
 	$(".alpha2").html(String(Math.round(sensors[i].alpha2)));
 	$(".alpha3").html(String(Math.round(sensors[i].alpha3)));
 	$(".alpha4").html(String(Math.round(sensors[i].alpha4)));
-	$(".temp").html(String(sensors[i].temp));
-	$(".rh").html(String(sensors[i].rh));
 	$("#lastupdated").html("Last Update: "+sensors[i].lastUpdated);
 };
 
-
-$(document).ready(function(){
-	RequestNodes();
-	var reset = setInterval(function() {RequestDatapoints()}, update_int);
-
-    //Leaflet Map
-    var googleLayer = new L.Google('ROADMAP',mapStylesArray);
-
-	var sWBound = L.latLng(42.365901,-71.079440);
-	var nEBound = L.latLng(42.350901,-71.207550);
-	var map = new L.Map('map', {center: [42.3590000, -71.095500], zoom: 16, minZoom: 14, maxBounds:[sWBound,nEBound], zoomControl: false, attributionControl: false, layers: [googleLayer] });
-	//map.setView([42.3590000, -71.095500], 16);
-
-	map.addLayer(googleLayer);
-	var zoomBar = L.control.zoom({ position: 'topright' }).addTo(map);
-	var attribution = L.control.attribution({position: 'topright'}).addTo(map);
-
-	map.touchZoom.disable();
-	map.dragging.disable();
-	map.doubleClickZoom.disable();
-	map.scrollWheelZoom.disable();
-
-	//Nodes
-	function drawNodes(){
-		
-		for(var i=0; i<sensors.length; i++){
+function drawNodes(){
+	console.log("Drawing Nodes...");
+	for(var i=0; i<sensors.length; i++){
 		if(sensors[i].lat){
 			var delt_lat = 0.00015;
 			var delt_lon = 0.00028;
 			if(sensors[i].indoor){
+				console.log("Sensor is indoors");
 				sensors[i].circ = L.polygon([[sensors[i].lat+delt_lat,sensors[i].lon],[sensors[i].lat-delt_lat,sensors[i].lon+delt_lon],[sensors[i].lat-delt_lat,sensors[i].lon-delt_lon]],{
 	    			color: 'red',
 	    			fillColor: "#f03",
@@ -179,15 +156,21 @@ $(document).ready(function(){
 				}).addTo(map);
 			}
 			else{
-				sensors[i].circ = L.circle([sensors[i].lat,sensors[i].lon], 16, {
+				console.log("sensor is outdoors");
+				console.log(sensors[i].lat+" "+sensors[i].lon);
+				var placement = [42.361995,-71.097513];
+				cirlce = L.circle(placement,16).addTo(map);
+				console.log("circle placed");
+				sensors[i].circ = L.circle(placement, 16, {
 	    			color: 'red',
 	    			fillColor: "#f03",
 	    			fillOpacity: 0.5
 				}).addTo(map);
+				console.log("sensor added to map");
 			}
 
 			sensors[i].circ.number = i;
-			
+
 			sensors[i].circ.bindPopup(sensors[i].location, {closeButton: false,'offset': L.point(0,-5)});
 			sensors[i].circ.on('mouseover', function(evt) {
 				evt.target.openPopup();
@@ -207,11 +190,36 @@ $(document).ready(function(){
 			});
 
 		};
-		};
-	}
+	};
+	console.log("Nodes Drawn!");
+}
 
-	var draw = setTimeout(function() {drawNodes()}, 500);
 
+$(document).ready(function(){
+    //Leaflet Map 
+
+	var googleLayer = new L.Google('ROADMAP',mapStylesArray);
+
+	var sWBound = L.latLng(42.365901,-71.079440);
+	var nEBound = L.latLng(42.350901,-71.207550);
+	var map = new L.Map('map', {center: [42.3590000, -71.095500], zoom: 16, minZoom: 14, maxBounds:[sWBound,nEBound], zoomControl: false, attributionControl: false, layers: [googleLayer] });
+	//map.setView([42.3590000, -71.095500], 16);
+
+	map.addLayer(googleLayer);
+	var zoomBar = L.control.zoom({ position: 'topright' }).addTo(map);
+	var attribution = L.control.attribution({position: 'topright'}).addTo(map);
+
+	map.touchZoom.disable();
+	map.dragging.disable();
+	map.doubleClickZoom.disable();
+	map.scrollWheelZoom.disable();
+
+	
+	RequestNodes();
+	//drawNodes();
+	//var reset = setInterval(function() {RequestNodes()}, update_int);
+
+   
 	var mapBig = true;
 
 	function moveMap(){
