66b85aa986f8331edd44e3f946f2c0cdce8fd337
Catherine Cheng <chengcatherine91@gmail.com>
2014-05-06 10:15:09 -0400

    CAPTIONS!!
    CAPTIONS!!

--- js/howitworks.js
+++ js/howitworks.js
@@ -1,4 +1,11 @@
-.row {
-	padding-left: 0px;
-	padding-right: 0px;
-}
+$(window).load(function() {
+    $('img.caption').captionjs({
+    	'mode': 'stacked'
+    });
+
+	$(".items img").click(function(ev){
+		 var caption = $(ev.currentTarget).attr("data-caption");
+		 $("#image_wrap figcaption").text(caption);
+	})
+});
+
