4fd9f734afc584bdf22c2131a113d0d9aa5872c6
Catherine Cheng <chengcatherine91@gmail.com>
2014-05-06 15:11:26 -0400

    index page enter
    index page enter

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-$("#enterbtn").focus();
+
+$(document).ready(function(){
+	$(document).keypress(function(e){
+		if(e.which == 13) {
+				console.log('keyclicked');
+			$('#enterbtn').trigger('click');
+		}
+	});
+});
